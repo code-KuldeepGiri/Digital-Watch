8175b49354d8d7e3ad151e0c29f40245ac76d9ba
code-KuldeepGiri <166214161+code-KuldeepGiri@users.noreply.github.com>
2024-04-23 15:17:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function showTime(){
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var session = "PM";
+
+    if (h > 12) {
+        h = h-12;
+        // h = 13-12 = 1
+    }
+
+    if (h>=12){
+        session = "AM";
+    }
+
+    h = h < 10 ? "0" + h : h ;
+    m = m < 10 ? "0" + m : m ;
+    s = s < 10 ? "0" + s : s ;
+
+    var time  = h + " : " + m + " : " + s + " : " + session;
+    document.getElementsByTagName('h1')[0].innerText = time ;
+    setTimeout(showTime,1000);
+}
